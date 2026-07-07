5d0af2799fc635ca4f475ffde1623426783c9f07
pavan chaudhari <128274736+pavanchaudhar@users.noreply.github.com>
2026-07-07 18:17:46 +0530

    Delete mama directory

--- mama/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MamaConfig(AppConfig):
-    name = 'mama'
--- mama/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from mama import views
-
-urlpatterns = [
-   
-    path('', admin.site.urls),
-    path('', views.index,),
-]
